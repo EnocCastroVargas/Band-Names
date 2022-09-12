fc6250717ff6fb655ffd0ecbfd1b2dc84570b11f
Enoc Castro <enocco70@gmail.com>
2022-09-11 20:14:16 -0600

    useSocket created

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
 import { useEffect, useState } from 'react';
-import  io from 'socket.io-client';
 
 import { BandAdd } from './components/BandAdd';
 import { BandList } from './components/BandList';
+import { useSocket } from './hooks/useSocket';
 
-const connectSocketServer = () => {
-  
-  const socket = io.connect('http://localhost:8080', {
-    transports: ['websocket']
-  });
-
-  return socket;
-}
 
 function App() {
 
-  const [ socket ] = useState(connectSocketServer());
-  const [ online, setOnline ] = useState(false);
   const [bands, setBands] = useState([]);
 
-  useEffect(() => {
-    setOnline( socket.connected );
-  }, [socket]);
-  
-  useEffect(() => {
-
-    socket.on('connect', () => {
-      setOnline( true );
-    });
-
-  }, [socket]);
-  
-  useEffect(() => {
-
-    socket.on('disconnect', () => {
-      setOnline( false );
-    });
-
-  }, [socket]);
+  const { online, socket } = useSocket('http://localhost:8080');
 
   useEffect(() => {
 
